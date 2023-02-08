f7546786fa4a0ded270793728d2f83f2abb9cf45
Alcatraz323 <alcatraz32323@gmail.com>
2023-02-08 23:47:38 +0800

    clover: tablet adaption for pixel experience

--- device.mk
+++ device.mk
@@ -574,3 +574,7 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(DEVICE_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(DEVICE_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
+# Launcher overlay
+PRODUCT_PACKAGES += \
+    NexusLauncherTabletOverlay
